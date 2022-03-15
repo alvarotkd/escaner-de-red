b2b53dce93ca9fd6338621a69ebdbf1305b0bb28
spider <alvaro_tkd5@hotmail.com>
2022-03-15 12:06:50 -0300

    correcciones

--- scanner.py
+++ scanner.py
@@ -1,17 +1,17 @@
 from pydoc import cli
 from tabnanny import verbose
-from scapy.all import *
+import scapy.all as scapy
 
 
 target_ip = '192.168.1.1/24'
 
-arp =ARP(pdst = target_ip)
+arp = scapy.ARP(pdst = target_ip)
 
-ether =Ether(dst = "ff:ff:ff:ff:ff:ff" )
+ether = scapy.Ether(dst = "ff:ff:ff:ff:ff:ff" )
 
 paquete = ether/arp
 
-respuesta = srp(paquete, timeout = 2, verbose=0) [0]
+respuesta = scapy.srp(paquete, timeout = 2, verbose=0) [0]
 
 clients= []
 
