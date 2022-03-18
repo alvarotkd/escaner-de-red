1311936e858461194a86586bd13536986dc9bbff
spider <alvaro_tkd5@hotmail.com>
2022-03-18 20:31:19 -0300

    agrego mensajeria telegram

--- scanner.py
+++ scanner.py
@@ -1,9 +1,12 @@
 from pydoc import cli
 from tabnanny import verbose
 import scapy.all as scapy
+import bottele
 
 
-target_ip = '10.10.10.1/24'
+
+    
+target_ip = '192.168.12.1/24'
 
 arp = scapy.ARP(pdst = target_ip)
 
@@ -19,6 +22,17 @@ for enviado,recivido in respuesta:
     clients+= [[recivido.psrc,recivido.hwsrc]]
 
 totalIp=len(clients)
+b=0
 
 for i in range (totalIp):
+    if clients[i][1] == 'd0:67:e5:e7:1b:8d':
+        b=1
     print("IP: {} MAC: {}". format(clients[i][0], clients[i][1]))
+
+if b==1:
+    bottele.telegram_bot_sendtext("tamy conectada")
+else:
+    bottele.telegram_bot_sendtext("tamy no conectada")
+    
+
+
